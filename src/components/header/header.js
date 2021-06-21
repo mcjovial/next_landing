a8968eb4273afaf0454ed58287507ebf0fd791a1
mcjovial <emmanuel.elias.232317@unn.edu.ng>
2021-06-21 08:32:51 -0400

    header with logo

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -9,7 +9,29 @@ import menuItems from './header.data';
 
 export default function Header({ className }) {
   return (
-      <h1>Header</h1>
+    <header sx={styles.header} className={className} id="Header">
+      <Container sx={styles.container}>
+        <Logo src={LogoDark} />
+        <Flex as="nav" sx={styles.nav}>
+          {menuItems.map((menuItem, i) => (
+            <Link
+              activeClass="active"
+              to={menuItem.path}
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              key={i}
+            >
+              {menuItem.label}
+            </Link>
+          ))}
+        </Flex>
+        <Button className="donate__btn" variant="secondary" aria-label="Get started">
+          Get Started
+        </Button>
+      </Container>
+    </header>
   );
 }
 
